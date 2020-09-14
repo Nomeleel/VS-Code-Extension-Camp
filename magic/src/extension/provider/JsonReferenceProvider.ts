a9efd1630590539dd35a5ce0ca207bca1deedfc6
Nomeleel <choonlay@outlook.com>
2020-09-15 00:43:01 +0800

    Imp symbol and definition provider  happy path.

--- magic/src/extension/provider/JsonReferenceProvider.ts
+++ magic/src/extension/provider/JsonReferenceProvider.ts
@@ -1,4 +1,6 @@
-import {CancellationToken, LocationLink, DefinitionProvider, Location, Position, Range, ReferenceContext, ReferenceProvider, TextDocument, Uri, workspace, DebugConsoleMode} from "vscode";
+import * as path from "path";
+import {CancellationToken, LocationLink, DefinitionProvider, Location, Position, Range, ReferenceContext, 
+  ReferenceProvider, TextDocument, Uri, workspace, SymbolInformation, commands, SymbolKind} from "vscode";
 
 export class JsonReferenceProvider implements DefinitionProvider, ReferenceProvider {
 
@@ -11,30 +13,51 @@ export class JsonReferenceProvider implements DefinitionProvider, ReferenceProvi
     if (match) {
       console.log(match[1]);
 
-      // 这里通过SymbolProvider 获取到目标文件的Uri
-      let testUri = Uri.file('/Users/choonlay/Desktop/test2.json');
-
-      let locationLink = {
-        originSelectionRange: textRange,
-        targetRange: await this.getFillRange(testUri),
-        targetUri: testUri,
-        targetSelectionRange: new Range(new Position(0, 0), new Position(0, 0))
-      } as LocationLink;
-
-      return [locationLink];
+      let symbol = await this.getSymbol(targetText, document);
+      if (symbol) {
+        let locationLink = {
+          originSelectionRange: textRange,
+          targetRange: symbol.location.range,
+          targetUri: symbol.location.uri,
+          targetSelectionRange: new Range(new Position(0, 0), new Position(0, 0))
+        } as LocationLink;
+  
+        return [locationLink];
+      }
     }
-
-    return undefined;
   }
 
-  private async getFillRange(uri: Uri) : Promise<Range>{
-    let textDocument = await workspace.openTextDocument(uri.fsPath);
-    let endPosition = new Position(textDocument.lineCount + 1, 0);
-
-    return new Range(new Position(0, 0), endPosition);
+  // 这里通过SymbolProvider 获取到目标文件的Uri
+  // 大写的结果由Dart插件提供 小写的会由这个插件实现
+  // 大写kind搜索Class 小写搜索File
+  // 小写的File可能会有很多 需要根据当前document uri中关键字 选取最接近的一个
+  private async getSymbol(query: string, textDocument: TextDocument ) : Promise<SymbolInformation | undefined> {
+    let symbolsList = await getWorkspaceSymbols(query);
+    symbolsList.forEach(console.log);
+    if (symbolsList) {
+      if (/[A-Z]\S*/.test(query)) {
+        return symbolsList.find((e) => e.kind === SymbolKind.Class);
+      } else {
+        let symbols = symbolsList.filter((e) => e.kind === SymbolKind.File);
+        if (symbols) {
+          if (symbols.length === 1) {
+            return symbols[0];
+          } else {
+            let paths = textDocument.uri.fsPath.split(path.sep);
+            let uriKey = paths[paths.length - 5];
+            return symbols.find((e) => e.containerName === uriKey);
+          }
+        }
+      }
+    }
   }
 
   public async provideReferences(document: TextDocument, position: Position, context: ReferenceContext, token: CancellationToken): Promise<Location[] | undefined> {
     return undefined;
   }
+}
+
+export async function getWorkspaceSymbols(query: string): Promise<SymbolInformation[]> {
+	const workspaceSymbolResult = await (commands.executeCommand("vscode.executeWorkspaceSymbolProvider", query) as Thenable<SymbolInformation[]>);
+	return workspaceSymbolResult || [];
 }
--- magic/src/extension/provider/SymbolProvider.ts
+++ magic/src/extension/provider/SymbolProvider.ts
@@ -1,12 +1,28 @@
-import { CancellationToken, SymbolKind, SymbolInformation, WorkspaceSymbolProvider, Location, Uri, Position } from "vscode";
+import { CancellationToken, SymbolKind, SymbolInformation, WorkspaceSymbolProvider, Location, Uri, Position, workspace, Range } from "vscode";
+import { downloadAndUnzipVSCode } from "vscode-test";
 
 export class SymbolProvider implements WorkspaceSymbolProvider {
   
+  // 动态的去查 大写搜索dart（这个可以交给Dart插件去做） 小写搜索json
+  // 文件在不断变化，如果一开始就把映射建立好，后期就要监听文件的变化，并维护映射
+  // 每次查询通过查找文件名或许不会太慢
   public async provideWorkspaceSymbols(query: string, token: CancellationToken): Promise<SymbolInformation[] | undefined> {
-    if (query === 'Hello') {
-      return [new SymbolInformation('Hello', SymbolKind.File, 'Test Hello', new Location(Uri.file(''), new Position(0, 0)))];
-    } 
+    if (/[a-z]\S*/.test(query)) {
+      let uris = await workspace.findFiles(`**/*${query}.json`);
+      if (uris) {
+        if (uris.length === 1) {
+          return [new SymbolInformation(query, SymbolKind.File, query, new Location(uris[0], await this.getFillRange(uris[0])))];
+        } else {
+          // 在containerName中添加标示
+        }
+      }
+    }
+  }
 
-    return undefined;
+  private async getFillRange(uri: Uri) : Promise<Range>{
+    let textDocument = await workspace.openTextDocument(uri.fsPath);
+    let endPosition = new Position(textDocument.lineCount + 1, 0);
+
+    return new Range(new Position(0, 0), endPosition);
   }
 }
