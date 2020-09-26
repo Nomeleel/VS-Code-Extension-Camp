4fcd5e1979987bcd0f20bb84fde5ceb9c89831fa
Nomeleel <choonlay@outlook.com>
2020-09-26 18:30:12 +0800

    ScriptFileListener happy path.

--- magic/src/extension/listener/ScriptFileListener.ts
+++ magic/src/extension/listener/ScriptFileListener.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as vs from "vscode";
-import { Uri } from "vscode";
+import { Position, TextDocument, TextEdit, Uri, window, workspace, WorkspaceEdit } from "vscode";
 
 export class ScriptFileListener implements vs.Disposable {
 	private readonly subscriptions: vs.Disposable[] = [];
@@ -20,11 +20,123 @@ export class ScriptFileListener implements vs.Disposable {
 	}
 
 	private scriptFileChangedHandle(event: any, operationType: OperationType) {
+		let getTargetFile = this.targetFileProvider(event, operationType);
+		console.log(getTargetFile);
+		console.log(getTargetFile.isNotEmpty());
+		if (getTargetFile.isNotEmpty()) {
+			vs.window.showInformationMessage('😊 😊 😊 Whether to update InitScript.dart based on your changes? 😊 😊 😊',
+				'Sure', 'No, Thanks').then((select) => {
+					if (select === 'Sure') {
+						this.updateInitScript(getTargetFile);
+					}
+				});
+		}
+	}
+	
+	private targetFileProvider(event: any, operationType: OperationType) : TargetFiles {
+		switch(operationType) {
+			default :
+				return this.getTargetFile(event.files);
+		}
+	}
 
+	private getTargetFile(files: Array<Uri>): TargetFiles {
+		let targetFiles = new TargetFiles();
+
+		files.forEach(file => {
+			let analysis = this.analysisFileUri(file);
+			if (analysis) {
+				targetFiles.pushAddedFile(analysis);
+			}
+		});
+
+		return targetFiles;
+	}
+
+	private async updateInitScript(targetFiles: TargetFiles): Promise<void> {
+		console.log(targetFiles.AddedFileMap);
+		let workspaceEdit = new WorkspaceEdit();
+		let textEdits = Array<TextEdit>();
+		
+		//let textDocument = await workspace.openTextDocument(targetFiles.AppJsonPath);
+		let lineCount = await this.realLineCount(targetFiles.AppJsonPath);
+		console.log(lineCount);
+
+		targetFiles.AddedFileMap?.forEach((name) => {
+			textEdits.push(TextEdit.insert(new Position(lineCount - 1, 0), `  dynamicCodeMap['${name}'] = init${name};\n`));
+			textEdits.push(TextEdit.insert(new Position(0, 0), `import '${name}.dart';\n`));
+		});
+
+		workspaceEdit.set(Uri.file(targetFiles.AppJsonPath), textEdits);
+		await workspace.applyEdit(workspaceEdit);
+		await workspace.saveAll();
+
+		await this.organizeImports(targetFiles.AppJsonPath);
+	}
+
+	private async organizeImports(path: string) {
+		await vs.commands.executeCommand('_dart.organizeImports', await workspace.openTextDocument(path));
+	}
+
+	private async realLineCount(path: string) : Promise<number> {
+		let textDocument = await workspace.openTextDocument(path);
+		let lineCount: number = textDocument.lineCount;
+		console.log(lineCount);
+		while(textDocument.lineAt(lineCount - 1).isEmptyOrWhitespace) {
+			lineCount--;
+		}
+		return lineCount;
+	}
+
+	private analysisFileUri(fileUri: Uri): AnalysisFileUri | undefined {
+		let fsPath: string = fileUri.fsPath;
+		let parsedPath = path.parse(fsPath);
+		if (parsedPath.ext === '.dart') {
+			let initScriptPath = path.resolve(fsPath, '../InitScript.dart');
+			if (fs.existsSync(initScriptPath)) {
+				return new AnalysisFileUri(initScriptPath,
+					parsedPath.dir.split(path.sep).pop() ?? '', parsedPath.name);
+			}
+		}
 	}
 
 	public dispose() {
 		this.subscriptions.forEach((s) => s.dispose());
+	}
+}
+
+class TargetFiles {
+	AppJsonPath!: string;
+	AddedFileMap!: Array<string>;
+	DeletedFileMap!: Map<string, Array<string>>;
+	UpdatedFileMap!: Map<string, Array<{ old: string, new: string }>>;
+
+	public pushAddedFile(analysisFileUri: AnalysisFileUri) : void {
+		this.AppJsonPath = analysisFileUri.AppJsonPath;
+		if (this.AddedFileMap === undefined) {
+			this.AddedFileMap = Array<string>();
+		}
+		this.AddedFileMap?.push(analysisFileUri.Name);
+	}
+
+	public isNotEmpty(): boolean {
+		return this.AppJsonPath !== undefined && this.AppJsonPath.length > 0 &&
+			((this.AddedFileMap !== undefined && this.AddedFileMap.length > 0) ||
+			(this.DeletedFileMap !== undefined && this.DeletedFileMap.size > 0) ||
+			(this.UpdatedFileMap !== undefined && this.UpdatedFileMap.size > 0));
+	}
+
+}
+
+class AnalysisFileUri {
+	AppJsonPath!: string;
+	Dir!: string;
+	Name!: string;
+
+	constructor(AppJsonPath: string, Dir: string, Name: string) {
+		this.AppJsonPath = AppJsonPath;
+		this.Dir = Dir;
+		this.Name = Name;
 	}
 }
 
