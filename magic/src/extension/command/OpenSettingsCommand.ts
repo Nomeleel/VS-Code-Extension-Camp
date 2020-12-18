3a348b0264e102d6c5ba173c14c6c3141ba429ae
Nomeleel <choonlay@outlook.com>
2020-12-18 14:44:44 +0800

    为所有配置项添加打开设置并定位到配置的命令

--- magic/src/extension/command/OpenSettingsCommand.ts
+++ magic/src/extension/command/OpenSettingsCommand.ts
@@ -1,18 +1,43 @@
-import { commands, Disposable } from "vscode";
+import { commands, Disposable, workspace } from "vscode";
 
 export class OpenSettingsCommand implements Disposable {
 	private disposables: Disposable[] = [];
 
 	constructor() {
-		this.disposables.push(
-		  commands.registerCommand("magic.openSettings", this.openSettings, this),
-		);
+		let settingsKeyArray = this.settingsKeyArray();
+		settingsKeyArray.forEach((e) => {
+			this.disposables.push(
+				commands.registerCommand(`magic.open${e}Settings`, () => this.openSettings(e)),
+			);
+		});
   }
 
-  private openSettings() {
+  private openSettings(key: string) {
+		console.log(key);
     // TODO(Nomeleel): 当作常量提取出去
-    commands.executeCommand("workbench.action.openSettings", "@ext:Nomeleel.magic fieldArray");
+    commands.executeCommand("workbench.action.openSettings", `@ext:Nomeleel.magic ${key}`);
   }
+
+	public settingsKeyArray() : Array<string>{
+		let settingsKeyArray = [''];
+		let configuration = workspace.getConfiguration('magic');
+		for (const key in configuration) {
+			if (configuration.has(key)) {
+				let sectionKey = this.firstCaseToUpper(key);
+				settingsKeyArray.push(sectionKey);
+				let subKeys : Object = configuration.get(key) ?? [];
+				// TODO(Nomeleel): 配置约束为两级 所以这里这样写 不使用递归
+				for (const subKey in subKeys) {
+					settingsKeyArray.push(`${sectionKey}${this.firstCaseToUpper(subKey)}`);
+				}
+			}
+		}
+		return settingsKeyArray;
+	}
+
+	public firstCaseToUpper(str: string) : string {
+		return str ? str.replace(str[0], str[0].toUpperCase()) : '';
+	}
 
   public dispose(): any {
 		this.disposables.forEach((e) => e.dispose());
